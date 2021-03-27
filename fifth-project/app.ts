bc0152690cd8ac2722933898053bed63e8fc7ce2
Gino Mortillero <g.mortillero@arcanys.com>
2021-03-27 17:56:16 +0800

    Getter

--- fifth-project/app.ts
+++ fifth-project/app.ts
@@ -126,7 +126,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
     
 }
-
+ 
 class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     titleInputElement: HTMLInputElement;
     desciptionInputElement: HTMLInputElement;
@@ -199,6 +199,14 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     private project: Project;
+
+    get persons() {
+        if (this.project.people == 1) {
+            return '1 person';
+        } else {
+            return `${this.project.people} persons`;
+        }
+    }
     
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
@@ -212,7 +220,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned.';
         this.element.querySelector('p')!.textContent = this.project.description;
 
     };
