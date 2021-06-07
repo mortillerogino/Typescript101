2d309b59e32fc697a3c32ed46cdffad3243f1e73
Gino Mortillero <g.mortillero@arcanys.com>
2021-06-07 20:55:33 +0800

    api key remove

--- sixth-project/src/app.ts
+++ sixth-project/src/app.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyBAJ-JPcFSPwGlUtlk4tQrAFKV4BwX9U8E";
+const GOOGLE_API_KEY = "test";
 
 declare var google: any;
 
